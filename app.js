a97d261a37e45f442c63d9862fdcf327808c1029
takumhonde9 <tmhonde45@gmail.com>
2020-11-12 22:26:38 -0800

    add home routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+
 import { config } from "./config.js";
+import { shop } from "./routes/shop.js";
 
 const app = express();
 // path not defined
@@ -14,6 +16,9 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 // set up public directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/", shop);
 
 app.listen(config.port, () => {
   console.log(`server listening on port: ${config.port}`);
