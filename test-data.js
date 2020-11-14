6de0c561e8901d36ba3463ca0f884033d4cb8247
takumhonde9 <tmhonde45@gmail.com>
2020-11-13 16:28:17 -0800

    add test data for products

---
+++ test-data.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    title: "raspberry cupcake",
+    price: 4.99,
+  },
+  {
+    id: 2,
+    title: "vanilla cupcake",
+    price: 4.99,
+  },
+  {
+    id: 3,
+    title: "blueberry cupcake",
+    price: 4.99,
+  },
+  {
+    id: 4,
+    title: "almond cupcake",
+    price: 4.99,
+  },
+  {
+    id: 5,
+    title: "coconut cupcake",
+    price: 4.99,
+  },
+  {
+    id: 6,
+    title: "thyme cupcake",
+    price: 5.99,
+  },
+  {
+    id: 7,
+    title: "earl grey cupcake",
+    price: 5.99,
+  },
+  {
+    id: 8,
+    title: "london fog cupcake",
+    price: 5.99,
+  },
+];
