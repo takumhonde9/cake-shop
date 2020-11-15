694eb3ab68d4bb612ab8369d9d8862ca50e98d94
takumhonde9 <tmhonde45@gmail.com>
2020-11-14 21:38:50 -0800

    add event handling for when cart button pressed

---
+++ public/javascripts/actions.js
@@ -0,0 +1,19 @@
+let state = {
+  cartDropdownActive: false,
+};
+
+$(document).ready(() => {
+  const cartButton = $(".cart-button button");
+  const cartDropdown = $(".cart-dropdown");
+
+  // CART BUTTON -> Click
+  cartButton.on("click", () => {
+    state.cartDropdownActive ? cartDropdown.hide() : cartDropdown.show();
+    // toggle state
+    state = { ...state, cartDropdownActive: !state.cartDropdownActive };
+  });
+
+  // PLUS BUTTON -> Click
+
+  // MINUS BUTTON -> Click
+});
