50b52bfe8496e74e79dfb33326425cce65f3943a
takumhonde9 <tmhonde45@gmail.com>
2020-11-14 19:42:32 -0800

    add cart button and cart dropdowns

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,11 @@
 import { STATUS } from "../constants.js";
-import { products } from "../test-data.js";
+import { products, cart } from "../test-data.js";
 
 export const getShop = (req, res) => {
   return res.status(STATUS.OK).render("shop", {
     pageTitle: "Baked",
     path: "/",
     products,
+    cart,
   });
 };
--- test-data.js
+++ test-data.js
@@ -2,41 +2,70 @@ export const products = [
   {
     id: 1,
     title: "raspberry cupcake",
+    desc: "A tasty cupcake",
     price: 4.99,
   },
   {
     id: 2,
     title: "vanilla cupcake",
+    desc: "A tasty cupcake",
     price: 4.99,
   },
   {
     id: 3,
     title: "blueberry cupcake",
+    desc: "A tasty cupcake",
     price: 4.99,
   },
   {
     id: 4,
     title: "almond cupcake",
+    desc: "A tasty cupcake",
     price: 4.99,
   },
   {
     id: 5,
     title: "coconut cupcake",
+    desc: "A tasty cupcake",
     price: 4.99,
   },
   {
     id: 6,
     title: "thyme cupcake",
+    desc: "A tasty cupcake",
     price: 5.99,
   },
   {
     id: 7,
     title: "earl grey cupcake",
+    desc: "A tasty cupcake",
     price: 5.99,
   },
   {
     id: 8,
     title: "london fog cupcake",
+    desc: "A tasty cupcake",
     price: 5.99,
   },
 ];
+
+export const cart = {
+  items: [
+    {
+      id: 8,
+      title: "london fog cupcake",
+      desc: "A tasty cupcake",
+      price: 5.99,
+      quantity: 2,
+    },
+    {
+      id: 7,
+      title: "earl grey cupcake",
+      desc: "A tasty cupcake",
+      price: 5.99,
+      quantity: 1,
+    },
+  ],
+  total: 17.97,
+  quantity: 3,
+};
