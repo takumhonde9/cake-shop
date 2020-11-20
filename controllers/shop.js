3e3de3543cbea2769e80d8dd8ee6901ed13fefcb
takumhonde9 <tmhonde45@gmail.com>
2020-11-19 21:18:17 -0800

    update path for shop controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,10 @@
-import { STATUS } from "../constants.js";
+import { PATH, STATUS } from "../constants.js";
 import { products, cart } from "../test-data.js";
 
 export const getShop = (req, res) => {
   return res.status(STATUS.OK).render("shop", {
     pageTitle: "Baked",
-    path: "/",
+    path: PATH.SHOP,
     products,
     cart,
   });
